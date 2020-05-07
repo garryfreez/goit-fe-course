6c40677f845ff871a068610c8782e558888c791f
Garry Freez <skirow96@gmail.com>
2020-05-07 19:01:11 +0300

    rewrite task 1 in hw 1

--- js/hw-01/task-1.js
+++ js/hw-01/task-1.js
@@ -1,22 +1,10 @@
 'use strict';
 
-// let name = 'Генератор защитного поля';
-// let price = 1000;
+let name = 'Генератор защитного поля';
+let price = 1000;
 
-// console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
 
-// price = 2000;
+price = 2000;
 
-// console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
-
-const clients = ['Mango', 'Ajax', 'Poly'];
-
-for (const client of clients) {
-    console.log(client);
-}
-
-const string = 'javascript';
-
-for (const character of string) {
-    console.log(character);
-}
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
